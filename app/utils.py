f2351e3e2af2d315c049044342e04f262eba4b69
Muzaffer Dede <muzaffer.dede@tcstock.com.tw>
2024-11-15 09:42:43 +0800

    added hash

--- app/utils.py
+++ app/utils.py
@@ -5,3 +5,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash(password: str) -> str:
     return pwd_context.hash(password)
 
+def verify(password, hashed) -> bool:
+    return pwd_context.verify(password, hashed)
+
